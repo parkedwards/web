4abe753d895bdb54ff3c0b5f3c27fe6effdc0d95
Edward Park <ed.sh.park@gmail.com>
2017-01-31 16:31:15 -0800

    exported app + queue objects earlier in the code for access to importing modules

--- routes/contact.js
+++ routes/contact.js
@@ -1,25 +1,32 @@
 var contactModel = require('../models/contact.js'),
-	express = require('express'),
-	router = express.Router(),
-	path = require('path'),
-	queue = require('kue');
+  express = require('express'),
+  router = express.Router(),
+  path = require('path')
 
-console.log('['+ __dirname + '/contact.js] okay');
+const { queue } = require('../app');
+
+console.log('[' + __dirname + '/contact.js] okay');
 
 router.post('/', function (req, res, next) {
-  
-  console.log('inside contact route!');
 
-	var formData = req.body;
-	console.log("user form received: ", formData);
+  var formData = req.body;
+  console.log("user form received: ", formData);
 
-	var job = queue.create('contact', {
-		title: 'Ditt.io Contact Form Submission',
-		to: 'contact@ditt.io',
-		formData: formData
-		}).save( function(err){
-			if( !err ) console.log( job.id );
-		});
+  var job = queue.create('contact', {
+    title: 'Ditt.io Contact Form Submission',
+    to: 'contact@ditt.io',
+    formData: formData
+  }).save(function (err) {
+    if (err) {
+      console.error(err);
+      return res.status(400).end();
+    }
+    else {
+      console.log('success!');
+      return res.status(201).end();
+    }
+  });
+
 });
 
 module.exports = router;
