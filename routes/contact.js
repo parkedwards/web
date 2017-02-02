cec34ca85dc62656ee15f54c534e19815d99e1d4
Edward Park <ed.sh.park@gmail.com>
2017-02-01 18:25:37 -0800

    altered job object format to keep it one level deep

--- routes/contact.js
+++ routes/contact.js
@@ -9,23 +9,46 @@ console.log('[' + __dirname + '/contact.js] okay');
 
 router.post('/', function (req, res, next) {
 
-  var formData = req.body;
-  console.log("user form received: ", formData);
+  let {
+    name,
+    email,
+    url,
+    comment,
+  } = req.body;
+
+  console.log("user form received: ", req.body);
 
   var job = queue.create('contact', {
     title: 'Ditt.io Contact Form Submission',
-    to: 'contact@ditt.io',
-    formData: formData
+    from: 'contact@ditt.io',
+    name,
+    email,
+    url,
+    comment,
   }).save(function (err) {
     if (err) {
       console.error(err);
       return res.status(400).end();
     }
     else {
-      console.log('job added!');
-      return res.status(201).end();
+      console.log('contact form submission added to MQ!');
     }
   });
+  // other chainable methods:
+  // can also set attempts ( job.attempts() )
+  // can also set priority ( job.priority('high') )
+
+  job.on('complete', () => {
+    console.log(`job for ${req.body.email} completed!`);
+  });
+
+  job.on('failed', () => {
+    console.error(`job for ${req.body.email} failed!`);
+  });
+
+  // redis size keeps filling up...might have to do with the extra listeners being applied with each route call
+
+  return res.status(201).end();
 
 });
 
